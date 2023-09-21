8c8d4ab945a25cc4685b19a4bed5af8911ce6691
DonovanPS <86479462+Julieth-Becerra@users.noreply.github.com>
2023-09-21 11:44:33 -0500

    Implementación consulta de filtro users

--- src/main/java/com/example/listore/repository/ListoreUserRepository.java
+++ src/main/java/com/example/listore/repository/ListoreUserRepository.java
@@ -2,12 +2,23 @@ package com.example.listore.repository;
 
 import com.example.listore.models.Credential;
 import com.example.listore.models.ListoreUser;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ListoreUserRepository extends GeneralRepository<ListoreUser>{
+
+    @Query("SELECT U FROM ListoreUser AS U WHERE U.role NOT IN('A')" +
+            "AND (:#{#user.name} is null or U.name LIKE  %:#{#user.name}%) " +
+            "AND (:#{#user.role} is null or U.role LIKE %:#{#user.role}%) " )
+    List<ListoreUser> findByFilter(
+            @Param("user") ListoreUser user,
+            Pageable page
+    );
 
     @Query("SELECT u FROM ListoreUser u WHERE u.credential = :credential")
     public ListoreUser findByCredential(@Param("credential") Credential credential);
