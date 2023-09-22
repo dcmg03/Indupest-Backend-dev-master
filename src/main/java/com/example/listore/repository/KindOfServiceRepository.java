d5ba252fcef38707f22d05e02eb4982e3c54d4a0
Julieth-Becerra <julieth.becerra01@uptc.edu.co>
2023-09-22 15:22:30 -0500

    Kind Of Service actualizacion

--- src/main/java/com/example/listore/repository/KindOfServiceRepository.java
+++ src/main/java/com/example/listore/repository/KindOfServiceRepository.java
@@ -1,8 +1,15 @@
 package com.example.listore.repository;
 
 import com.example.listore.models.KindOfService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface KindOfServiceRepository extends GeneralRepository<KindOfService>{
+    @Override
+        @Query("select K from KindOfService as K where K.service.id = :#{#kindOfService.service.id}")
+    List<KindOfService> findByFilter(KindOfService kindOfService, Pageable page);
 }
